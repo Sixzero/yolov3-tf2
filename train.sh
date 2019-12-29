ee634b7734011559fe93671d80b505f11a7fc1f3
SixZero <havliktomi@hotmail.com>
2019-12-29 14:04:28 +0100

    Minor changes.

---
+++ train.sh
@@ -0,0 +1,5 @@
+
+python train.py --batch_size 1 --dataset /Users/tamashavlik/repo/test/records/Screens-TFRecords-export/*.tfrecord --epochs 100 --mode fit --transfer none --tiny
+# python3 train.py --batch_size 8 --dataset /Users/tamashavlik/repo/test/records/Screens-TFRecords-export/1ff88ed89985e015974f519f5e9e6a2e.tfrecord --epochs 100 --mode eager_tf --transfer fine_tune
+
+
